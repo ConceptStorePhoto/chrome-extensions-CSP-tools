f99577f720df544ddd918308640e163f2c4a8d8c
ConceptStorePhoto <shopphoto.percepied@gmail.com>
2025-06-20 18:37:30 +0200

    ajout popup groupe toogle

--- popup.js
+++ popup.js
@@ -34,7 +34,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // fonction pour gérer les toogles
     const toggleButtons = document.querySelectorAll('.toggle-button');
     toggleButtons.forEach(button => {
-        let id = button.id;
+        const id = button.id;
+        const groupe = button.dataset.groupe;
 
         // Charger l'état actuel pour chaque bouton
         chrome.storage.sync.get(id, (data) => {
@@ -43,6 +44,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Mise à jour de l'état 
         button.addEventListener('change', () => {
+            // Si le bouton fait partie d'un groupe
+            if (groupe && button.checked) {
+                // Désactiver les autres boutons du même groupe
+                toggleButtons.forEach(other => {
+                    if (other !== button && other.dataset.groupe === groupe && other.checked) {
+                        other.checked = false;
+                        other.dispatchEvent(new Event('change'));
+                    }
+                });
+            }
             chrome.storage.sync.set({ [button.id]: button.checked }, () => {
                 chrome.runtime.sendMessage({ type: "updateContextMenu" });
             });
