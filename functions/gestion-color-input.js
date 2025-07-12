d7439aa36fef94137358a812ca3a8819bb6b0a1a
ConceptStorePhoto <shopphoto.percepied@gmail.com>
2025-07-12 12:32:56 +0200

    patch color input

--- functions/gestion-color-input.js
+++ functions/gestion-color-input.js
@@ -8,12 +8,15 @@ export function gestionColorInput() {
         chrome.storage.sync.get([id, id_default], (data) => {
             colorInput.value = data[id] || data[id_default];
             defaultColor = data[id_default];
+            // if (!data[id])  // masque le bouton de réinitialisation si pas de couleur personnalisée
+            //     colorInput.parentElement.querySelector('.reset-btn').classList.add('hide');
         });
         // Enregistrer la nouvelle couleur quand elle change
-        colorInput.addEventListener('input', () => {
+        colorInput.addEventListener('change', () => {
             const nouvelleCouleur = colorInput.value;
             chrome.storage.sync.set({ [id]: nouvelleCouleur }, () => {
                 console.log("🎨 Couleur de remplacement enregistrée :", nouvelleCouleur);
+                // colorInput.parentElement.querySelector('.reset-btn').classList.remove('hide');
             });
         });
         // Clic sur le bouton de réinitialisation
@@ -21,6 +24,7 @@ export function gestionColorInput() {
             colorInput.value = defaultColor;
             chrome.storage.sync.remove(id, () => {
                 console.log('🎨 Couleur personnalisée supprimée ; retour au réglage par défaut.');
+                // colorInput.parentElement.querySelector('.reset-btn').classList.add('hide');
             });
         });
     });
