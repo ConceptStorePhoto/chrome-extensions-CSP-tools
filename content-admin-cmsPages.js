bbdc8e35c92b9851c63300e367312aa3f887045c
ConceptStorePhoto <shopphoto.percepied@gmail.com>
2025-10-09 18:44:16 +0200

    MAJ cmsPage script

--- content-admin-cmsPages.js
+++ content-admin-cmsPages.js
@@ -8,7 +8,7 @@ document.querySelectorAll('.column-link_rewrite').forEach(cell => {
     cell.innerHTML = `<a href="${lienPage}" target="_blank">${originalText}</a>`;
 
     const bouton = document.createElement("button");
-    bouton.innerText = "📋";
+    bouton.innerText = "📋lien";
     bouton.title = "Copier le lien";
     bouton.className = "copier-btn";
     bouton.style.marginLeft = "8px";
@@ -21,9 +21,29 @@ document.querySelectorAll('.column-link_rewrite').forEach(cell => {
         event.preventDefault();
         event.stopPropagation(); // évite les comportements attachés ailleurs
         navigator.clipboard.writeText(lienPage).then(() => {
-            bouton.innerText = "✅";
-            setTimeout(() => (bouton.innerText = "📋"), 1500);
+            bouton.innerText = "✅lien";
+            setTimeout(() => (bouton.innerText = "📋lien"), 1500);
         });
     };
     cell.appendChild(bouton);
+
+    const bouton2 = document.createElement("button");
+    bouton2.innerText = "📋/chemin";
+    bouton2.title = "Copier le chemin";
+    bouton2.className = "copier-btn";
+    bouton2.style.marginLeft = "8px";
+    bouton2.style.borderRadius = "10px";
+    bouton2.style.border = "none";
+    bouton2.style.outline = "none";
+    bouton2.style.backgroundColor = "#e7e7e7";
+    bouton2.style.padding = "4px 8px";
+    bouton2.onclick = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        navigator.clipboard.writeText(`/content/${id}-${originalText}`).then(() => {
+            bouton2.innerText = "✅/chemin";
+            setTimeout(() => (bouton2.innerText = "📋/chemin"), 1500);
+        });
+    };
+    cell.appendChild(bouton2);
 });
